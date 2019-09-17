9207f40d0dae4998ed0f2d8f7737176fc3898c7d
hcabezasm11 <hcabezasm11@gmail.com>
2019-09-17 17:42:45 -0400

    Clase - 15

---
+++ resources/views/admin/menu/crear.blade.php
@@ -0,0 +1,29 @@
+@extends("theme.$theme.layout")
+@section('titulo')
+    Sistema Menus
+@endsection
+@section('contenido')
+    <div class="row">
+        <div class="col-md-12">
+            @include('includes.form-error')
+            @include('includes.mensaje')
+            <div class="box box-danger">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Crear Menu</h3>
+                </div>
+                <form action="{{route('guardar_menu')}}" id="form-general" class="form-horizontal" method="POST">
+                    @csrf
+                    <div class="box-body">
+                        @include('admin.menu.form')
+                    </div>
+                    <div class="box-footer">
+                        <div class="col-lg-3"></div>
+                        <div class="col-lg-6">
+                            @include('includes.boton-form-crear')
+                        </div>
+                    </div>
+                </form>
+            </div>        
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,19 @@
 use App\Http\Controllers\Admin\PermisoController;
 
 Route::get('/', 'InicioController@index');
-// Route::get('admin/permiso', 'Admin\PermisoController@index')->name('permiso');
-Route::group(['prefix' => 'admin','namespace'=> 'Admin'], function () {
-    Route::get('permiso', 'PermisoController@index')->name('permiso');
-    Route::get('permiso/crear', 'PermisoController@crear')->name('crear_permiso');
-});
+Route::get('admin/permiso', 'Admin\PermisoController@index')->name('permiso');
+Route::get('admin/permiso/crear', 'Admin\PermisoController@crear')->name('crear_permiso');
+Route::get('admin/menu', 'Admin\MenuController@index')->name('menu');
+Route::get('admin/menu/crear', 'Admin\MenuController@crear')->name('crear_menu');
+Route::post('admin/menu', 'Admin\MenuController@guardar')->name('guardar_menu');
+
+// Route::group(['prefix' => 'admin','namespace'=> 'Admin'], function () {
+//     Route::get('permiso', 'PermisoController@index')->name('permiso');
+//     Route::get('permiso/crear', 'PermisoController@crear')->name('crear_permiso');
+//     Route::get('menu', 'MenuController@index')->name('menu');
+//     Route::get('menu/crear', 'MenuController@crear')->name('crear_menu');
+//     Route::post('menu', 'MenuController@guardar')->name('guardar_menu');
+// });
 // Route::get('/', function () {
 //     return view('welcomess');
 // });
